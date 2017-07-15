86bbb923260b16270d30b35f8235a96337383d26
皮皮虾 <pipixia@qq.com>
2017-07-15 20:44:36 +0800

    完成了basic页面的保存按钮功能

--- views/public/js/course/basic.js
+++ views/public/js/course/basic.js
@@ -1,7 +1,40 @@
 /**
  * Created by Administrator on 2017/7/15.
  */
-define(function (){
+define(['utils','jquery','template','ckeditor','form'],function (obj,$,template,CKEDITOR,form){
    //要根据传过来的id，获取当前的所有的信息，渲染到当前页面上来
+    var id = obj.queryString().cs_id;
+    $.ajax({
+      url:'/api/course/basic',
+      type:'get',
+      data:{
+        cs_id:id
+      },
+      success:function (info){
+          if(info.code==200){
+            var htmlStr = template('cs_basic_tpl',info.result);
+            $('.steps').html(htmlStr);
 
+            //添加富文本编辑器
+            CKEDITOR.replace('cs_brief');
+          }
+      }
+    });//ajax
+
+    // 给保存按钮注册事件
+  $('.steps').on('click','#btnSave',function (){
+         // alert(123);
+    $("#cs_brief").val(CKEDITOR.instances.cs_brief.getData());
+    $('form').ajaxSubmit({
+      url:'/api/course/update/basic',
+      type:'post',
+      success:function (info){
+         if(info.code==200){
+           alert('保存成功，即将跳转到图片中心...');
+          location.href='/course/pic?cs_id='+info.result.cs_id;
+         }
+      }
+    })
+         return false;
+  })
 })
