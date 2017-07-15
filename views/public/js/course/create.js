4a5f9e23c58e3316730181a4c0030f775e12ac67
皮皮虾 <pipixia@qq.com>
2017-07-15 20:00:32 +0800

    课程添加操作完成

---
+++ views/public/js/course/create.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/7/15.
+ */
+define(['jquery','form'],function ($){
+     // alert(123);
+  //将当前添加的课程名称内容，提交给服务器的数据库
+  $('#btnCreate').on('click',function (){
+    $('form').ajaxSubmit({
+      url:'/api/course/create',
+      type:'post',
+      success:function (info){
+        if(info.code==200){
+          alert('提交成功');
+          location.href='/course/basic';
+        }
+      }//success
+    })//ajaxSubmit
+    return false; //阻止默认的刷新 行为
+  })//on
+
+})
