59544c66e67d9a6db8a88b7c59490700e104b60d
皮皮虾 <pipixia@qq.com>
2017-07-17 21:02:02 +0800

    课时管理中的编辑功能完成

--- views/public/js/course/lesson.js
+++ views/public/js/course/lesson.js
@@ -11,13 +11,37 @@ define(['utils','jquery','template','bootstrap','form'],function (utils,$,templa
   $('.steps').on('click','#addLesson',function (){
       var htmlStr = template('cs_modal_tpl',{
         title:'课时添加',
-        savaBtnText:'添加'
+        savaBtnText:'添加',
+        actionUrl:'/api/course/chapter/add'
       });
       $('#editPanel').html(htmlStr);
          $('#lesson').modal();
   });
 
   //3. 当我们单击编辑按钮的时候，要显示当前课程的所有信息在模态框上
+  $('.steps').on('click','#editBtn',function (){
+         //把当前章节的内容 信息，渲染在模板 上
+    var ct_id = $(this).parent().attr('data-id');
+      $.ajax({
+        url:'/api/course/chapter/edit',
+        type:'get',
+        data:{
+          ct_id:ct_id
+        },
+        success:function (info){
+          info.result.title = '课时编辑';
+          info.result.savaBtnText = '保存';
+          info.result.actionUrl = '/api/course/chapter/modify'
+           if(info.code==200){
+             var htmlStr= template('cs_modal_tpl',info.result);
+             $('#editPanel').html(htmlStr);
+
+             //显示模态框
+             $('#lesson').modal();
+           }
+        }
+      });
+  })
 
   //4. 单击保存按钮的时候，要把当前的信息内容提交给服务器
   $('#editPanel').on('click','#saveBtn',function (){
@@ -25,7 +49,7 @@ define(['utils','jquery','template','bootstrap','form'],function (utils,$,templa
     var ct_is_free  =Number($('#ct_is_free').prop('checked')); //将复选框的状态，转换成number类型
          // ct  chapter 章节
     $('form').ajaxSubmit({
-      url:'/api/course/chapter/add',
+      // url:'/api/course/chapter/add',
       type:'post',
       data:{
         ct_is_free:ct_is_free,
@@ -33,7 +57,7 @@ define(['utils','jquery','template','bootstrap','form'],function (utils,$,templa
       },
       success:function (info){
         if(info.code==200){
-          alert('课程添加成功');
+          alert('操作成功');
 
           // 重新渲染页面
           renderLesson();
